b9e507736607ca6e1d690c7a9e4d6b97373983f5
qhero70 <qhero70@gmail.com>
2026-06-01 12:15:58 +0800

    新增 PWA service worker

---
+++ smart-factory-command-center/02_HTML前端/service-worker.js
@@ -0,0 +1,12 @@
+const 快取名稱 = '智慧製造中央作戰指揮中心-v1';
+const 必要檔案 = ['./index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(快取名稱).then(cache => cache.addAll(必要檔案)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request).then(res => res || caches.match('./index.html')))
+  );
+});
