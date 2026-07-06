6c22b4a54e3b73b7fd7913ef4716f2b61ca573ab
qhero70 <qhero70@gmail.com>
2026-07-06 12:16:30 -0700

    test: add temporary loader file

---
+++ docs/tmp-v491-loader-test.js
@@ -0,0 +1,5 @@
+/* temporary loader test */
+(function(){
+function load(){var s=document.createElement('script');s.src='./work-report-v4-zh-merge-491.js?v=491';s.id='tmpTest';document.head.appendChild(s)}
+setTimeout(load,10);
+})();
