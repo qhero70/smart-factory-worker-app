3a02fd27c94bab860729dd4ade7e0b1016d226ca
qhero70 <qhero70@gmail.com>
2026-07-06 12:41:31 -0700

    remove temporary v491 loader test

--- docs/tmp-v491-loader-test.js
+++
@@ -1,5 +0,0 @@
-/* temporary loader test */
-(function(){
-function load(){var s=document.createElement('script');s.src='./work-report-v4-zh-merge-491.js?v=491';s.id='tmpTest';document.head.appendChild(s)}
-setTimeout(load,10);
-})();
